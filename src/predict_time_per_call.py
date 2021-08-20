1e7d4f23a84af18e04bd28287878124f964c6923
ClaudioZeni <claudiopuntozeni@gmail.com>
2021-08-20 09:30:29 +0200

    Added threads per node and npool optimizer

--- src/predict_time_per_call.py
+++ src/predict_time_per_call.py
@@ -14,22 +14,34 @@ def optimize(model, nel, nsp, nat, ntrans, nlanth, nks, ngsmooth,
     ncores = nnodes*corespernode
     npools = 1+np.arange(nnodes)
     x_ = np.array([a for a in itertools.product(tpns, npools)])
-    x = np.array([nel, nel**3, nsp, nat, ntrans, nlanth, nks,
-                  ngsmooth, nk, nbetas, ncores, nnodes, arch])
-    X = np.empty((len(x_), len(x)+x_.shape[1]))
-    X[:, 12:14] = x_
-    X[:, :12] = x[:-1]
-    X[:, :-1] = x[-1]
-
+    X = np.zeros((len(x_), 13+x_.shape[1]))
+    X[:, 12] = x_[:, 0]
+    X[:, 13] = x_[:, 1]
+    X[:, :12] = np.array([nel, nel**3, nsp, nat, ntrans, nlanth, nks,
+                          ngsmooth, nk, nbetas, ncores, nnodes])
+    X[:, -1] = arch
     X = pd.DataFrame(X)
     X.columns = ['n_el', 'n_el^3', 'n_species', 'n_at', 'n_transition',
                  'n_lanthanid',
                  'n_ks', 'n_g_smooth', 'n_k', 'n_betas', 'n_cores', 'n_nodes',
                  'threads_per_node', 'n_pool', 'arch']
-    Y = model.predict_normed(X)
-    t = Y*nel**3*nk/ncores
+    Y = model.predict_normed(X)[:, 0]
+    t = Y*(nel**3)*nk/ncores
     tmin = np.min(t)
     opt_tpn = x_[np.argmin(t), 0]
     opt_npool = x_[np.argmin(t), 1]
 
     return tmin, opt_tpn, opt_npool
+
+
+def optimize_from_df(model, df):
+    df['corespernode'] = df['n_cores']//df['n_nodes']
+    cols = ['n_el', 'n_species', 'n_at', 'n_transition',
+            'n_lanthanid', 'n_ks', 'n_g_smooth', 'n_k',
+            'n_betas', 'n_nodes', 'corespernode', 'arch']
+    if type(df) == pd.DataFrame:
+        return optimize(model, *df[:, cols].values)
+    elif type(df) == pd.Series:
+        return optimize(model, *df[cols].values)
+    elif type(df) == dict:
+        return optimize(model, *df[cols])
